831cbb4f3d930373f6a40b65de08c4f6d321613d
Arjun Iyer <iarjun2001@gmail.com>
2024-09-16 21:13:43 -0400

    Trapping Rain Water

--- src/com/arjun/interviewprep/Tester.java
+++ src/com/arjun/interviewprep/Tester.java
@@ -7,8 +7,8 @@ import java.util.List;
 public class Tester {
     public static void main(String[] args){
 
-        MaxWaterContainerSolution solution = new MaxWaterContainerSolution();
-        int[] nums = {1,7,2,5,4,7,3,6};
-        System.out.println(solution.maxArea(nums));
+        TrappingRainWaterSolution solution = new TrappingRainWaterSolution();
+        int[] nums = {0,2,0,3,1,0,1,3,2,1};
+        System.out.println(solution.trap(nums));
     }
 }
